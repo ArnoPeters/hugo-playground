0153499480ada7e77ca708288e46f8ac27fe609a
Arno Peters <arno.peters@gmail.com>
2022-11-27 15:14:34 +0100

    Cleanup

--- tests/redirect-login-paths.spec.ts
+++ tests/redirect-login-paths.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('When unauthenticated', () => {
-    test.use({ storageState: 'tests/emptyStorageState.json' }); 
-  test('twitter should redirect to password', async ({ page }) => {
-      await page.goto('/.auth/login/twitter');
-      await expect(page).toHaveURL(/basicAuth/);
-  });
-  test('github should redirect to password', async ({ page }) => {
-        await page.goto('/.auth/login/github');
-      await expect(page).toHaveURL(/basicAuth/);
-  });
-  test('aad should redirect to password', async ({ page }) => {
-      await page.goto('/.auth/login/aad');
-      await expect(page).toHaveURL(/basicAuth/);
-  });
+	test.use({ storageState: 'tests/emptyStorageState.json' });
+	test('twitter should redirect to password', async ({ page }) => {
+		await page.goto('/.auth/login/twitter');
+		await expect(page).toHaveURL(/basicAuth/);
+	});
+	test('github should redirect to password', async ({ page }) => {
+		await page.goto('/.auth/login/github');
+		await expect(page).toHaveURL(/basicAuth/);
+	});
+	test('aad should redirect to password', async ({ page }) => {
+		await page.goto('/.auth/login/aad');
+		await expect(page).toHaveURL(/basicAuth/);
+	});
 });
